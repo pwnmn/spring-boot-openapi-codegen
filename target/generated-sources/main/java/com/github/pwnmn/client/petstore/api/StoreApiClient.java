2497b39a034edba37876610044a916fffd3111a1
Peter Weismann <peter.weismann@integrationworks.co.th>
2020-11-29 19:19:36 +0700

    Add gitignore

--- target/generated-sources/main/java/com/github/pwnmn/client/petstore/api/StoreApiClient.java
+++
@@ -1,8 +0,0 @@
-package com.github.pwnmn.client.petstore.api;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import com.github.pwnmn.client.petstore.config.ClientConfiguration;
-
-@FeignClient(name="${store.name:store}", url="${store.url:http://localhost/api/v3}", configuration = ClientConfiguration.class)
-public interface StoreApiClient extends StoreApi {
-}
